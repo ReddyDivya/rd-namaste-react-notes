7296b9fb9102a69cbd666ce0fa2873bf72842433
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-08-09 05:57:22 -0700

    Update App.js

--- Chapter 01 - Inception/Coding/App.js
+++ Chapter 01 - Inception/Coding/App.js
@@ -1,1 +1,18 @@
+//React.createElement is a JS Object
+const heading = React.createElement(
+    "div", //Which element is to be created?
+    {id : "parent"}, //Setting attributes to the element created
+    React.createElement("div", {id : "child"}, 
+    //Siblings in an array
+    [React.createElement("h1", {}, "I'm a h1 tag"),
+    React.createElement("h2", {}, "I'm a h2 tag")])
+);//what to display?
 
+//Rendering react
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+/*
+render() => renders a JS object(heading) and 
+creates the element which the browser understands and puts it up in the DOM.
+*/
+root.render(heading);
