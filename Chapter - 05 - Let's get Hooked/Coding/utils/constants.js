7964953094b61f21097b7068694196e0586ca728
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-08-19 09:47:40 -0700

    Create constants.js

---
+++ Chapter - 05 - Let's get Hooked/Coding/utils/constants.js
@@ -0,0 +1,2 @@
+export const LOGO_URL = "https://img.freepik.com/premium-vector/restaurant-logo-design-template_79169-56.jpg?w=2000";
+export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
