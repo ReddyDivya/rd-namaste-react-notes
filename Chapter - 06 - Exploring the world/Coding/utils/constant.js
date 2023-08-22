0970cc456709a594670be731be30abbb6df75256
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-08-22 01:15:07 -0700

    Create constant.js

---
+++ Chapter - 06 - Exploring the world/Coding/utils/constant.js
@@ -0,0 +1,3 @@
+export const LOGO_URL = "https://img.freepik.com/premium-vector/restaurant-logo-design-template_79169-56.jpg?w=2000";
+export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+export const SWIGGY_API_URL = "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
